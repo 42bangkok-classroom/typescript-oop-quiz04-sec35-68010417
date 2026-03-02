a3e22ceea2d6316cefec27f0a49590aac99dcc8a
68010417 <68010417@kmitl.ac.th>
2026-03-02 09:45:47 +0700

    p2_3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersService } from './users/users.service';
-import { UsersController } from './users/users.controller';
-import { UsersModule } from './users/users.module';
+import { UsersService } from './user/user.service';
+import { UsersController } from './user/user.controller';
+import { UsersModule } from './user/user.module';
 
 @Module({
   imports: [UsersModule],
